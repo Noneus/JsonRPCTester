41b05e4a81d1dbf08de512d38cde68412889acda
Stefan Lohmaier <stefan@noneus.com>
2015-01-04 22:04:00 +0100

    add syntax highlighters and connect send button slot

--- jsonrpctester/jsonrpctester.py
+++ jsonrpctester/jsonrpctester.py
@@ -8,8 +8,21 @@ from ui_jsonrpctester import Ui_MainWindow
 class JsonRpcTester(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(JsonRpcTester, self).__init__(parent)
+
+        #build ui
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        #set syntax highlighters
+        self.syntax_params = JSONSyntaxHighlighter(self.ui.textEditParameters)
+        self.syntax_result = JSONSyntaxHighlighter(self.ui.textEditResult)
+
+        #connect signals
+        self.ui.pushButtonSend.clicked.connect(self.send)
+
+    @QtCore.Slot()
+    def send(self):
+        pass
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
